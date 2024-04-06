e6ecafb367db659f56e13bf22b7f6287a9413bcc
shubham <mahatmashubham0@gmail.com>
2024-04-06 10:05:05 +0530

    Add Another Migration that associated with the city means city have many airports

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const apiRoutes = require('./routes');
 
 const App = express();
 
-App.use(express.json());
+App.use(express.json());  
 App.use(express.urlencoded({extended: true,}))
 
 App.use('/api', apiRoutes);
 
 
-App.listen(serverConfig.Port , ()=> {
+App.listen(serverConfig.Port , async ()=> {
     console.log(`Successfully started server on Port ${serverConfig.Port}`);
+    const {City , Airport} = require('./models')
+    // const city = await City.findByPk(14);
+    // await city.createAirport({name: 'Np Airport' , code : 'NP' , address: "nayapu Nagar"})
+    await City.destroy({
+        where: {
+            id: 14
+        }
+    });
 })
